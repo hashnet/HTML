7d2dc73ccb4b5aa445fae151f193e99aec7c7d88
hashnet <maidul.hasan@gmail.com>
2017-11-15 20:30:36 +0400

    Update

---
+++ AutoLogin/src/com/hashnet/LoginLogoutTest.java
@@ -0,0 +1,107 @@
+package com.hashnet;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+public class LoginLogoutTest {
+	private static final int TIME_OUT = 5000;
+	private static final String LOGIN_URL = "https://holdmycourt.com/reserve2/reserve_signin.php";
+	private static final String LOGOUT_URL = "https://holdmycourt.com/reserve2/reserve_signin.php?logout=1&dir=rahrc";
+	private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0";
+	private static final Map<String, String> HEADERS;
+	
+	static {
+		HEADERS = new HashMap<String, String>();
+		HEADERS.put("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+		//HEADERS.put("Referer", "https://holdmycourt.com/reserve2/reserve_signin.php?dir=rahrc");
+		HEADERS.put("Connection", "keep-alive");
+	}
+	
+	private static Document data;
+	private static Map<String, String> cookies;
+	
+	public static void main(String[] args) throws IOException {
+		if(login(LOGIN_URL)) {
+			System.out.println("Login Successful.");
+			System.out.println("Data:");
+			System.out.println(data);
+			
+			System.out.print("\n\n\n");
+			if(logout(LOGOUT_URL)) {
+				System.out.println("Logout Successful.");
+				System.out.println("Data:");
+				System.out.println(data);
+			} else {
+				System.out.println("Logout Unsuccessful");
+			}
+		} else {
+			System.out.println("Login Unsuccessful");
+		}
+
+	
+	}
+	
+	private static boolean login(String url) throws IOException {
+		Map<String, String> postData = new HashMap<String, String>();
+		postData.put("dir", "rahrc");
+		postData.put("date", "");
+		postData.put("hour", "");
+		postData.put("court", "");
+		postData.put("email", "maidul.hasan@gmail.com");
+		postData.put("password", "deyuxu");
+		postData.put("signin", "Sign In");
+		
+		Connection conn = Jsoup.connect(url)
+				.userAgent(USER_AGENT)
+				.headers(HEADERS)
+				.method(Connection.Method.POST)
+				.timeout(TIME_OUT)
+				.followRedirects(true)
+				.data(postData);
+		
+		Connection.Response response = conn.execute();
+		
+		if(response.statusCode() == 200) {
+			data = response.parse();
+		} else {
+			return false;
+		}
+	
+		if(response.hasCookie("reserve_rahrc")) {
+			cookies = response.cookies();
+		} else {
+			return false;
+		}
+		
+		return true;
+	}
+	
+	private static boolean logout(String url) throws IOException {
+		if(!cookies.containsKey("reserve_rahrc")) {
+			return false;
+		}
+		
+		Connection conn = Jsoup.connect(url)
+				.userAgent(USER_AGENT)
+				.headers(HEADERS)
+				.method(Connection.Method.GET)
+				.timeout(TIME_OUT)
+				.followRedirects(true)
+				.cookies(cookies);
+		
+		Connection.Response response = conn.execute();
+		
+		if(response.statusCode() == 200) {
+			data = response.parse();
+		} else {
+			return false;
+		}
+		
+		return true;
+	}
+}
--- AutoLogin/src/com/hashnet/LoginTest.java
+++ AutoLogin/src/com/hashnet/LoginTest.java
@@ -1,7 +1,59 @@
 package com.hashnet;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 public class LoginTest {
-	public static void main(String[] args) {
-		System.out.println("Test");
+	private static final int TIME_OUT = 5000;
+	private static final String LOGIN_URL = "https://holdmycourt.com/reserve2/reserve_signin.php";
+	private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0";
+	private static final Map<String, String> HEADERS;
+	private static final Map<String, String> POST_DATA;
+	static {
+		HEADERS = new HashMap<String, String>();
+		HEADERS.put("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+		HEADERS.put("Referer", "https://holdmycourt.com/reserve2/reserve_signin.php?dir=rahrc");
+		HEADERS.put("Connection", "keep-alive");
+		
+		POST_DATA = new HashMap<String, String>();
+		POST_DATA.put("dir", "rahrc");
+		POST_DATA.put("date", "");
+		POST_DATA.put("hour", "");
+		POST_DATA.put("court", "");
+		POST_DATA.put("email", "maidul.hasan@gmail.com");
+		POST_DATA.put("password", "deyuxu");
+		POST_DATA.put("signin", "Sign In");
+	}
+	
+	public static void main(String[] args) throws IOException {
+		
+		Connection conn = Jsoup.connect(LOGIN_URL)
+				.userAgent(USER_AGENT)
+				.headers(HEADERS)
+				.method(Connection.Method.POST)
+				.timeout(TIME_OUT)
+				.followRedirects(true)
+				.data(POST_DATA);
+		
+		Connection.Response response = conn.execute();
+	
+		System.out.println(response.parse());
+		
+		System.out.println(response.headers());
+		
+		System.out.println(response.statusCode());
+		
+		System.out.println(response.statusMessage());
+		
+		System.out.println(response.hasCookie("reserve_rahrc"));
+		
+		System.out.println(response.cookies());
+
+	
 	}
 }
